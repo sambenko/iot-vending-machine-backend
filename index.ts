b8da622b807b3e84ec37715bf06041d4fcb18fbf
sambenko <sam.benko@gmail.com>
2023-12-01 16:07:38 +0100

    mount endpoint

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import express from 'express';
+import router from './src/api/routes/routes';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,6 +10,30 @@ async function main() {
     console.log("Starting the application...");
     await runSample();
     console.log("MQTT Client is running. Press CTRL+C to exit.");
+
+    let app = express();
+    const port = 3001;
+
+    const cors = require('cors');
+
+    const corsOptions = {
+        origin: 'http://localhost:3000',
+        optionsSuccessStatus: 200
+    };
+    app.use(cors(corsOptions));
+    
+    app.use(express.json());
+    app.use('/api', router);
+
+    app.get('/', (req, res) => {
+        res.send('Backend running!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Express server running on http://localhost:${port}`);
+    });
+
+    
 
     process.on('SIGINT', () => {
         console.log("Received SIGINT. Gracefully shutting down.");
